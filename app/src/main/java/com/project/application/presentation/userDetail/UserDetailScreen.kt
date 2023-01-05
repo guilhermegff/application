80f4456c73f3d133d956332050199db1faceef54
Guilherme Freire <gfreire@MB.local>
2023-01-04 22:20:15 -0300

    Implement screen

--- app/src/main/java/com/project/application/presentation/userDetail/UserDetailScreen.kt
+++ app/src/main/java/com/project/application/presentation/userDetail/UserDetailScreen.kt
@@ -1,4 +1,4 @@
-package com.project.application.presentation.compose
+package com.project.application.presentation.userDetail
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
@@ -12,9 +12,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import com.project.application.presentation.UserDetailViewModel
+import com.project.application.presentation.compose.ErrorScreen
+import com.project.application.presentation.compose.LoadingScreen
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 internal fun UserDetailScreen(
     viewState: UserDetailViewModel.ViewState,
