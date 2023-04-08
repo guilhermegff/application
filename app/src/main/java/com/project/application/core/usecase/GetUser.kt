19233a07d8dbabe99759f77b785ccff505e1b45d
Guilherme Freire <gfreire@MB.local>
2023-04-08 20:31:14 -0300

    Removing the shared dependency

--- app/src/main/java/com/project/application/core/usecase/GetUser.kt
+++ app/src/main/java/com/project/application/core/usecase/GetUser.kt
@@ -1,18 +1,16 @@
 package com.project.application.core.usecase
 
-import com.example.shared.invoke
 import com.project.application.infrastructure.UserRepository
 import com.project.application.presentation.mapper.UserUiMapper
-import java.util.concurrent.Executor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetUser @Inject constructor(
-    private val executor: Executor,
     private val userRepository: UserRepository,
     private val userUiMapper: UserUiMapper,
 ) {
-    suspend operator fun invoke(id: String) = executor {
-        println("Executor - $executor")
+    suspend operator fun invoke(id: String) = withContext(Dispatchers.Default) {
         println("UseCase ${Thread.currentThread().name}")
         userUiMapper.transform((userRepository.getUser(id)))
     }
--- app/src/main/java/com/project/application/core/usecase/GetUserList.kt
+++ app/src/main/java/com/project/application/core/usecase/GetUserList.kt
@@ -1,18 +1,16 @@
 package com.project.application.core.usecase
 
-import com.example.shared.invoke
 import com.project.application.infrastructure.UserRepository
 import com.project.application.presentation.mapper.UserUiMapper
-import java.util.concurrent.Executor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetUserList @Inject constructor(
-    private val executor: Executor,
     private val userRepository: UserRepository,
     private val userUiMapper: UserUiMapper,
-)  {
-    suspend operator fun invoke() = executor {
-        println("Executor - $executor")
+) {
+    suspend operator fun invoke() = withContext(Dispatchers.Main) {
         println("UseCase ${Thread.currentThread().name}")
         userUiMapper.transform((userRepository.getUserList()))
     }
