88f9310b4af89f84c708e01468c1e91cb93229e0
Guilherme Freire <gfreire@MB.local>
2023-05-04 21:31:05 -0300

    Add navigation to module 3

--- module3-impl/src/main/java/com/example/module3_impl/Module3Activity.kt
+++ module3-impl/src/main/java/com/example/module3_impl/Module3Activity.kt
@@ -1,20 +1,15 @@
 package com.example.module3_impl
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import com.example.module3_impl.presentation.compose.Module3InitialScreen
+import androidx.appcompat.app.AppCompatActivity
+import com.project.module3_impl.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-internal class Module3Activity : ComponentActivity() {
+internal class Module3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Module3InitialScreen {
-
-            }
-        }
+        setContentView(R.layout.activity_module_3)
     }
 }
