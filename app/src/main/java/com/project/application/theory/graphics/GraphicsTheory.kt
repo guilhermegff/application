90aea37bf84f3054a43b7913d42d93aea6159e20
Guilherme Freire <gfreire@MB.local>
2023-08-07 12:49:24 -0300

    More on graphics

---
+++ app/src/main/java/com/project/application/theory/graphics/GraphicsTheory.kt
@@ -0,0 +1,25 @@
+package com.project.application.theory.graphics
+
+class GraphicsTheory {
+
+    /*
+    * The coordinates of the centroid of a triangle are found by averaging the x- and y-coordinates of the vertices.
+    * This method will also find the centroid (center of mass) of any set of points on the x-y plane.
+    * To find the centroid, follow these steps:
+    * Step 1: Identify the coordinates of each vertex.
+    * Step 2: Add all the x values from the three vertices coordinates and divide by 3 = 211.4 493.3.
+    * Step 3: Add all the y values from the three vertices coordinates and divide by 3 = 407.1 950.
+    * */
+
+    /*
+    * Affine transforms is a set of operations that provides for translation and
+    * all linear transformations like scale, rotate and shear.
+    *
+    * The affine transforms scale, rotate and shear can be represented by a matrix
+    * multiplication of a point represented as a vector.
+    *
+    * To be able to represent the translation also as a linear transform it is required
+    * to add homogeneous coordinates into the matrices and vector, so they become 3D.
+    *
+    * */
+}
